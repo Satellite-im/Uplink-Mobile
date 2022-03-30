387128fa931122a34209929646959c875bd1effd
Yijing Huang <hyj891204@gmail.com>
2022-03-30 16:41:58 -0500

    feat:set up uplink
    - set up flutter project with very good CLI
    - update app name and ID
    - delete original license(MIT)
    - set the minimum test coverage to 30 %  for the time being

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.satellite.uplink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:uplink/app/app.dart';
+import 'package:uplink/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
