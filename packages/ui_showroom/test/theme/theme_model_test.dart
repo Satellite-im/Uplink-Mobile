f4bc0dc5d1432afe7d492bad4763433fb992b86c
Yijing Huang <hyj891204@gmail.com>
2022-04-13 15:53:08 -0500

    test(ui_showroom/theme): add SharedPreferences and ThemeModel tests -FA-13

---
+++ packages/ui_showroom/test/theme/theme_model_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ui_library/core/core_export.dart';
+import 'package:ui_showroom/theme/theme_model.dart';
+
+void main() {
+  group('SharedPreferences', () {
+    test('return \'dark\'  when no theme name saved', () async {
+      SharedPreferences.setMockInitialValues({});
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final themeName = prefs.getString('themeName');
+
+      expect(themeName, null);
+    });
+
+    test('return \'light\'  when light theme name saved', () async {
+      SharedPreferences.setMockInitialValues({'themeName': 'light'});
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final themeName = prefs.getString('themeName');
+
+      expect(themeName, 'light');
+    });
+  });
+
+  group('ThemeModel', () {
+    ThemeModel themeModel;
+    test('return dark theme when no theme saved', () async {
+      themeModel = ThemeModel();
+      expect(themeModel.getThemeName, 'dark');
+      expect(themeModel.getThemeData, UThemes.dark);
+    });
+    test('get light theme when set theme name to \'light\'', () {
+      themeModel = ThemeModel();
+      themeModel.setTheme('light');
+
+      expect(themeModel.getThemeData, UThemes.light);
+    });
+
+    test('get dark theme when theme name is unavailable', () {
+      themeModel = ThemeModel();
+      themeModel.setTheme('olalala');
+
+      expect(themeModel.getThemeData, UThemes.dark);
+    });
+  });
+}
