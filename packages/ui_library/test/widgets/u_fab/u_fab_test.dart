4ddf7267ac100c315a733f3e2d77707fff202ad4
Yijing Huang <hyj891204@gmail.com>
2022-04-29 13:21:14 -0500

    test(UFAB):add golden test

---
+++ packages/ui_library/test/widgets/u_fab/u_fab_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ui_library/widgets/widgets_export.dart';
+import '../helper/golden_test_widget_page.dart';
+
+void main() {
+  group('UFAB:Golden Test', () {
+    testWidgets(
+      'UFAB.android',
+      (tester) async {
+        await tester.pumpWidget(
+            GoldenTestWidgetPage(widget: UFAB.android(onPressed: () {})));
+        await expectLater(
+            find.byType(UFAB), matchesGoldenFile('goldens/u_fab_android.png'));
+      },
+      tags: 'golden',
+    );
+    testWidgets(
+      'UFAB.ios',
+      (tester) async {
+        await tester.pumpWidget(
+            GoldenTestWidgetPage(widget: UFAB.ios(onPressed: () {})));
+        await expectLater(
+            find.byType(UFAB), matchesGoldenFile('goldens/u_fab_ios.png'));
+      },
+      tags: 'golden',
+    );
+    testWidgets(
+      'UFAB.big',
+      (tester) async {
+        await tester.pumpWidget(
+            GoldenTestWidgetPage(widget: UFAB.big(onPressed: () {})));
+        await expectLater(
+            find.byType(UFAB), matchesGoldenFile('goldens/u_fab_big.png'));
+      },
+      tags: 'golden',
+    );
+  });
+}
