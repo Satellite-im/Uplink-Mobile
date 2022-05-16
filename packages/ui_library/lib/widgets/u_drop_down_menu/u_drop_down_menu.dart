98dbb13fb6d20ac8635db5c081b7f3c2131aff9c
Yijing Huang <hyj891204@gmail.com>
2022-05-16 17:04:29 -0500

    feat(UDropDownMenu):extract UDropDownMenu widget and add docs -FA-28

---
+++ packages/ui_library/lib/widgets/u_drop_down_menu/u_drop_down_menu.dart
@@ -0,0 +1,101 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/material.dart';
+import 'package:ui_library/ui_library_export.dart';
+
+class UDropDownMenu extends StatefulWidget {
+  ///Create a UDropDownMenu
+  ///
+  ///[items] is a List of options(String)
+  ///
+  ///[selectedValue] is an optional String that shows the default selected option.
+  ///If it is null, it uses the first String of the [items]
+  ///
+  ///[getValueIndex] is a function to get the index of selected value in the List(options)
+  ///```dart
+  ///GlobalKey<UDropDownMenuState> uDropDownMenuStateKey = GlobalKey();
+  ///...
+  ///UDropDownMenu(
+  ///   key: uDropDownMenuStateKey,
+  ///   items: [
+  ///    'Satellite.im + Public Signaling Servers',
+  ///    'Title',
+  ///  ],
+  ///),
+  ///...
+  ///   UButton.filled1(
+  ///     label: 'Continue',
+  ///     onPressed: () {
+  ///        final _selectedValue =
+  ///               uDropDownMenuStateKey.currentState?.selectedValue;
+  ///        final _selectedValueIndex =
+  ///                uDropDownMenuStateKey.currentState?.getValueIndex();
+  ///        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+  ///                content: Text(
+  ///                    'Selected value: $_selectedValue \nThe Index of selected value: $_selectedValueIndex')));
+  ///          }),
+  ///```
+  const UDropDownMenu({
+    Key? key,
+    required this.items,
+    this.selectedValue,
+  }) : super(key: key);
+  final List<String> items;
+  final String? selectedValue;
+
+  @override
+  State<UDropDownMenu> createState() => UDropDownMenuState();
+}
+
+class UDropDownMenuState extends State<UDropDownMenu> {
+  late String? selectedValue = widget.selectedValue;
+
+  int getValueIndex() {
+    return widget.items.indexOf(selectedValue ?? widget.items.first);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2(
+        isDense: true,
+        items: widget.items
+            .map((item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Row(
+                    children: [
+                      UText(
+                        item,
+                        textStyle: UTextStyle.BUT1_primaryButton,
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                ))
+            .toList(),
+        value: selectedValue ??= widget.items.first,
+        selectedItemHighlightColor: UColors.ctaBlue,
+        onChanged: (value) {
+          setState(() {
+            selectedValue = value as String;
+          });
+        },
+        icon: const UIcon(UIcons.dropdown_menu),
+        buttonHeight: 40,
+        buttonPadding: const EdgeInsets.fromLTRB(25, 8, 16, 8),
+        buttonDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: UColors.ctaBlue,
+        ),
+        itemHeight: 48,
+        itemPadding: const EdgeInsets.only(left: 16),
+        dropdownPadding: null,
+        dropdownElevation: 0,
+        dropdownDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: UColors.foregroundDark,
+        ),
+        offset: const Offset(0, -8),
+      ),
+    );
+  }
+}
--- packages/ui_showroom/lib/ui_pages/src/onboarding_widgets/u_dropdown_menu_page.dart
+++ packages/ui_showroom/lib/ui_pages/src/onboarding_widgets/u_dropdown_menu_page.dart
@@ -1,6 +1,9 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_library/ui_library_export.dart';
+import 'package:ui_library/widgets/u_drop_down_menu/u_drop_down_menu.dart';
+
+GlobalKey<UDropDownMenuState> uDropDownMenuStateKey = GlobalKey();
 
 class UDropDownMenuPage extends StatefulWidget {
   const UDropDownMenuPage({Key? key}) : super(key: key);
@@ -11,7 +14,6 @@ class UDropDownMenuPage extends StatefulWidget {
 }
 
 class _UDropDownMenuPageState extends State<UDropDownMenuPage> {
-  String selectedValue = 'Satellite.im + Public Signaling Servers';
   List<String> items = [
     'Satellite.im + Public Signaling Servers',
     'Title',
@@ -25,48 +27,32 @@ class _UDropDownMenuPageState extends State<UDropDownMenuPage> {
       appBar: AppBar(
         title: Text(UDropDownMenuPage.routeName.substring(1)),
       ),
-      body: Center(
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton2(
-            isDense: true,
-            items: items
-                .map((item) => DropdownMenuItem<String>(
-                      value: item,
-                      child: Row(
-                        children: [
-                          UText(
-                            item,
-                            textStyle: UTextStyle.BUT1_primaryButton,
-                          ),
-                          const SizedBox(width: 8),
-                        ],
-                      ),
-                    ))
-                .toList(),
-            value: selectedValue,
-            selectedItemHighlightColor: UColors.ctaBlue,
-            onChanged: (value) {
-              setState(() {
-                selectedValue = value as String;
-              });
-            },
-            icon: const UIcon(UIcons.dropdown_menu),
-            buttonHeight: 40,
-            buttonPadding: const EdgeInsets.fromLTRB(25, 8, 16, 8),
-            buttonDecoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: UColors.ctaBlue,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: UDropDownMenu(
+                key: uDropDownMenuStateKey,
+                items: items,
+              ),
             ),
-            itemHeight: 48,
-            itemPadding: const EdgeInsets.only(left: 16),
-            dropdownPadding: null,
-            dropdownElevation: 0,
-            dropdownDecoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4),
-              color: UColors.foregroundDark,
-            ),
-            offset: const Offset(0, -8),
-          ),
+            const SizedBox(height: 56),
+            UButton.filled1(
+                label: 'Continue',
+                onPressed: () {
+                  final _selectedValue =
+                      uDropDownMenuStateKey.currentState?.selectedValue;
+                  final _selectedValueIndex =
+                      uDropDownMenuStateKey.currentState?.getValueIndex();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                          'Selected value: $_selectedValue \nThe Index of selected value: $_selectedValueIndex')));
+                })
+          ],
         ),
       ),
     );
