b3141caba6be92be0ce36388ab90278bf7b96eff
Yijing Huang <hyj891204@gmail.com>
2022-05-19 12:43:36 -0500

    chore: delete counter sample app and its test -FA-34

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,14 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ui_showroom/ui_showroom_page.dart';
 import 'package:uplink/app/app.dart';
-import 'package:uplink/counter/counter.dart';
+import 'package:uplink/pages_export.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
       final _counterPageOrUIShowroomPage = find.byWidgetPredicate(
-        (widget) => widget is CounterPage || widget is UIShowRoomApp,
+        (widget) => widget is OnboardPinPage || widget is UIShowRoomApp,
       );
       expect(_counterPageOrUIShowroomPage, findsOneWidget);
     });
