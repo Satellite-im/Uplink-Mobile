39f11b60340441879fdad3ca718d6a464b2f834b
lgmarchi <lucasgmarchi@gmail.com>
2022-05-24 15:27:47 -0300

    FA-37 - refactor(RecoverySeedBoxPage): Refactor screenshot to work better on IOS

--- lib/auth/onboard_recovery_seed_page/screenshot_recovery_seed_boxes_part.dart
+++ lib/auth/onboard_recovery_seed_page/screenshot_recovery_seed_boxes_part.dart
@@ -5,24 +5,32 @@ class _ScreenShotRecoverySeedBoxes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-      child: Column(
-        children: const [
-          SizedBox.square(
-            dimension: 32,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: UColors.backgroundDark,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            child: Column(
+              children: const [
+                SizedBox.square(
+                  dimension: 32,
+                ),
+                UText(
+                  'Save this in a safe place or write it down. To recover your '
+                  'account, you’ll need your 12 passphrases '
+                  'in the order that they '
+                  'appear. Please do not share these passphrases with anybody.',
+                  textStyle: UTextStyle.B1_body,
+                ),
+                SizedBox.square(
+                  dimension: 24,
+                ),
+                _RecoverySeedBoxes(),
+              ],
+            ),
           ),
-          UText(
-            'Save this in a safe place or write it down. To recover your '
-            'account, you’ll need your 12 passphrases in the order that they '
-            'appear. Please do not share these passphrases with anybody.',
-            textStyle: UTextStyle.B1_body,
-          ),
-          SizedBox.square(
-            dimension: 24,
-          ),
-          _RecoverySeedBoxes(),
-        ],
+        ),
       ),
     );
   }
