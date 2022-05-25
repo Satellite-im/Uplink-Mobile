56126fae878a191cb7f0a8ca3475beeae4c1586c
lgmarchi <lucasgmarchi@gmail.com>
2022-05-25 10:54:37 -0300

    FA-37 - feat(CreateProfilePage): build create profile page

--- lib/auth/onboarding_create_profile_page.dart
+++ lib/auth/onboarding_create_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:ui_library/ui_library_export.dart';
 
 class OnboardCreateProfilePage extends StatefulWidget {
@@ -11,6 +12,19 @@ class OnboardCreateProfilePage extends StatefulWidget {
 
 class _OnboardCreateProfilePageState extends State<OnboardCreateProfilePage> {
   final _focusNode = FocusNode();
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels > 40 &&
+          _scrollController.position.userScrollDirection ==
+              ScrollDirection.reverse) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      }
+    });
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -23,58 +37,101 @@ class _OnboardCreateProfilePageState extends State<OnboardCreateProfilePage> {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: UAppBar.back(title: 'Create Profile'),
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-            child: ListView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              children: [
-                const SizedBox(height: 30),
-                const UText(
-                  'Create Profile',
-                  textStyle: UTextStyle.H1_primaryHeader,
-                ),
-                const SizedBox(height: 16),
-                const UText(
-                  'Sed ut perspiciatis unde omnis iste natus error sit '
-                  'voluptatem accusantium doloremque laudantium.',
-                  textStyle: UTextStyle.B1_body,
-                ),
-                const SizedBox(height: 40),
-                Align(
-                  child: UUserPictureChange(
-                    onPictureSelected: (pictureSelected) {
-                      print(
-                        'AQUI: TESTE ******* ####### ---------- ${pictureSelected?.path}',
-                      );
-                    },
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              reverse: true,
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 16),
+                  const UText(
+                    'Sed ut perspiciatis unde omnis iste natus error sit '
+                    'voluptatem accusantium doloremque laudantium.',
+                    textStyle: UTextStyle.B1_body,
                   ),
-                ),
-                const SizedBox(height: 56),
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: UText(
-                    'Username',
-                    textStyle: UTextStyle.H3_tertiaryHeader,
+                  const SizedBox(height: 40),
+                  Align(
+                    child: UUserPictureChange(
+                      onPictureSelected: (pictureSelected) {},
+                    ),
                   ),
-                ),
-                const SizedBox.square(dimension: 8.4),
-                TextField(
-                  textInputAction: TextInputAction.newline,
-                  // controller: widget.textEditingController,
-                  // focusNode: _focusNode,
-                  cursorColor: UColors.textDark,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: UColors.foregroundDark,
-                    border: InputBorder.none,
-                    hintText: 'Digit your username...',
-                    hintStyle:
-                        UTextStyle.H5_fifthHeader.style.returnTextStyleType(),
+                  const SizedBox(height: 56),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16),
+                    child: UText(
+                      'Username',
+                      textStyle: UTextStyle.H3_tertiaryHeader,
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox.square(dimension: 8.4),
+                  SizedBox(
+                    height: 48,
+                    child: TextField(
+                      textInputAction: TextInputAction.next,
+                      cursorColor: UColors.textDark,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: UColors.foregroundDark,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        hintText: 'Digit your username...',
+                        hintStyle: UTextStyle.H5_fifthHeader.style
+                            .returnTextStyleType(),
+                      ),
+                    ),
+                  ),
+                  const SizedBox.square(dimension: 24),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16),
+                    child: UText(
+                      'Status Message',
+                      textStyle: UTextStyle.H3_tertiaryHeader,
+                    ),
+                  ),
+                  const SizedBox.square(dimension: 8.4),
+                  SizedBox(
+                    height: 48,
+                    child: TextField(
+                      textInputAction: TextInputAction.done,
+                      cursorColor: UColors.textDark,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: UColors.foregroundDark,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        hintText: 'Digit your status message...',
+                        hintStyle: UTextStyle.H5_fifthHeader.style
+                            .returnTextStyleType(),
+                      ),
+                    ),
+                  ),
+                  const SizedBox.square(dimension: 56),
+                  UButton.primary(
+                    label: 'Sign in',
+                    uIconData: UIcons.friend_added,
+                    onPressed: () {},
+                  ),
+                  AnimatedPadding(
+                    duration: const Duration(milliseconds: 50),
+                    curve: Curves.easeInOut,
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
