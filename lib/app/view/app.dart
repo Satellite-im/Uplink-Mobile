2e4f89a55b64d65ced7b875f4a6fd6db62ef9771
lgmarchi <lucasgmarchi@gmail.com>
2022-05-26 10:20:10 -0300

    FA-65 feat(ULoadingIndicator): Create Loading Indicator

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -13,7 +13,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const _appToBuild = Apps.mainApp;
+    const _appToBuild = Apps.uiShowroom;
 
     return ChangeNotifierProvider(
       create: (context) => ThemeModel(),
