ca01cbc99e2424ec9c664bc6de00b45ccedc8997
Yijing Huang <hyj891204@gmail.com>
2022-06-08 17:32:40 -0500

    feat: add no friends in contacts pages -UL-71

--- lib/contacts/contacts_index_page.dart
+++ lib/contacts/contacts_index_page.dart
@@ -46,9 +46,88 @@ class _ContactsIndexPageState extends State<ContactsIndexPage> {
         ),
         title: UAppStrings.contactsIndexPage_appBarTitle,
       ),
-      body: const SafeArea(
-        child: Text(''),
+      body: FutureBuilder(
+        future: _loadingContacts(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final friendList = snapshot.data! as List<Friends>;
+            if (friendList.isEmpty) {
+              return const NoFriendBody();
+            } else {
+              return ListView.builder(
+                itemBuilder: (context, index) =>
+                    Text(friendList[index].toString()),
+                itemCount: friendList.length,
+              );
+            }
+          }
+          return const Center(child: ULoadingIndicator());
+        },
       ),
     );
   }
+}
+
+class NoFriendBody extends StatelessWidget {
+  const NoFriendBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ...const [
+            Center(
+              child: UText(
+                'No Friends Yet',
+                textStyle: UTextStyle.H1_primaryHeader,
+              ),
+            ),
+            SizedBox(height: 16),
+            UText(
+              'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
+              textStyle: UTextStyle.B1_body,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 56),
+          ],
+          UButton.primary(
+            label: 'Add a Friend',
+            uIconData: UIcons.add_contact_member,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// TODO(yijing): update loading contacts
+Future<List<Friends>> _loadingContacts() async {
+  const hasFriends = false;
+  if (hasFriends == true) {
+    return Future.delayed(const Duration(seconds: 1), () {
+      final result = [
+        Friends(name: 'test', statusMessage: 'test', isOnline: true),
+      ];
+      return result;
+    });
+  }
+  return [];
+}
+
+class Friends {
+  Friends({
+    required this.name,
+    required this.statusMessage,
+    required this.isOnline,
+  });
+  String name;
+  String statusMessage;
+  bool isOnline;
 }
