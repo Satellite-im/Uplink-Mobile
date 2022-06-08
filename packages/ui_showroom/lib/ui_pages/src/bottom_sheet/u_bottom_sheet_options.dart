85b1092152da3c4b5ecaaf7d7e73f96bbe8f28f6
Yijing Huang <hyj891204@gmail.com>
2022-06-08 13:00:19 -0500

    chore:move UBottomSheetOptions to ui_library and showroom -UL-69

---
+++ packages/ui_showroom/lib/ui_pages/src/bottom_sheet/u_bottom_sheet_options.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:ui_library/ui_library_export.dart';
+
+class UBottomSheetOptionsPage extends StatelessWidget {
+  const UBottomSheetOptionsPage({Key? key}) : super(key: key);
+
+  static const routeName = '/UBottomSheetOptions';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(routeName.substring(1)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: ListView(
+            children: [
+              const UText(
+                'Bottom Sheet with multiple options',
+                textStyle: UTextStyle.H1_primaryHeader,
+              ),
+              const SizedBox.square(
+                dimension: 8,
+              ),
+              UButton.filled1(
+                  label: 'Click here to see the bottom sheet',
+                  onPressed: () {
+                    UBottomSheetOptions(
+                      context,
+                      sheetTitle: 'More Options',
+                      titleList: ['Switch Account', 'New Account'],
+                      iconList: [
+                        UIcons.multiple_accounts,
+                        UIcons.add_contact_member
+                      ],
+                      onTapList: [
+                        () {
+                          // TODO(yijing): add workflow later
+                        },
+                        () {
+                          // TODO(yijing): add workflow later
+                        },
+                      ],
+                    ).show();
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
