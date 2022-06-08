b503dba230f3355903e6a4f4cc37398336285174
Yijing Huang <hyj891204@gmail.com>
2022-06-08 12:24:14 -0500

    test:update widget type -UL-69

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,14 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ui_showroom/ui_showroom_page.dart';
 import 'package:uplink/app/app.dart';
-import 'package:uplink/pages_export.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders First Page', (tester) async {
       await tester.pumpWidget(const App());
       final _mainAppPageOrUIShowroomPage = find.byWidgetPredicate(
-        (widget) => widget is OnboardPinPage || widget is UIShowRoomApp,
+        (widget) => widget is MaterialApp || widget is UIShowRoomApp,
       );
       expect(_mainAppPageOrUIShowroomPage, findsOneWidget);
     });
