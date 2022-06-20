fbd9887c07df40fa846dfd67c39d645d50c5d8c2
lgmarchi <lucasgmarchi@gmail.com>
2022-06-20 17:19:04 -0300

    UL-88 - feat(ChatIndexPage): Build the first chat page

--- lib/chat/chat_index_page.dart
+++ lib/chat/chat_index_page.dart
@@ -46,8 +46,111 @@ class _ChatIndexPageState extends State<ChatIndexPage> {
         ),
         title: UAppStrings.chatIndexPage_appBarTitle,
       ),
-      body: const SafeArea(
-        child: Text(''),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const _FavoritesList(),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const UText(
+                      'Start a Conversation',
+                      textStyle: UTextStyle.H1_primaryHeader,
+                      textColor: UColors.textMed,
+                    ),
+                    const SizedBox.square(
+                      dimension: 16,
+                    ),
+                    const Center(
+                      child: UText(
+                        'Sed ut perspiciatis unde omnis iste natus error '
+                        'sit voluptatem accusantium doloremque laudantium.',
+                        textStyle: UTextStyle.B1_body,
+                        textColor: UColors.white,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox.square(
+                      dimension: 56,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: UButton.primary(
+                        label: 'Add a Friend',
+                        uIconData: UIcons.add_contact_member,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _FavoritesList extends StatelessWidget {
+  const _FavoritesList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Column(
+        children: [
+          const SizedBox.square(
+            dimension: 17,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: UText(
+              'Favorites',
+              textStyle: UTextStyle.H5_fifthHeader,
+              textColor: UColors.textMed,
+            ),
+          ),
+          const SizedBox.square(
+            dimension: 16,
+          ),
+          const _NewChatButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class _NewChatButton extends StatelessWidget {
+  const _NewChatButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32),
+      child: Column(
+        children: [
+          UIconButton.add(onPressed: () {}),
+          const SizedBox.square(
+            dimension: 8,
+          ),
+          const UText(
+            'New Chat',
+            textStyle: UTextStyle.M1_micro,
+            textColor: UColors.white,
+          ),
+        ],
       ),
     );
   }
