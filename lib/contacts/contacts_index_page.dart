ba8dabde6dd0c2f856e34d6f002b0b7940cc6715
lgmarchi <lucasgmarchi@gmail.com>
2022-06-22 18:19:26 -0300

    UL-72 - refactor(ContactsIndexPage): Make the AppBar in ContactsIndexPage scrollable

--- lib/contacts/contacts_index_page.dart
+++ lib/contacts/contacts_index_page.dart
@@ -15,169 +15,95 @@ class ContactsIndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: UAppBar.actions(
-        title: UAppStrings.contactsIndexPage_appBarTitle,
-        leading: IconButton(
-          icon: const UIcon(
-            UIcons.lefthand_navigation_drawer,
-            color: UColors.textMed,
-          ),
-          onPressed: () {},
-        ),
-        actionList: [
-          IconButton(
-            onPressed: () {
-              showCustomSearch(
-                context: context,
-                delegate: ContactSearch(loadingContacts()),
-              );
-            },
-            icon: const UIcon(
-              UIcons.search,
-              color: UColors.textMed,
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (context) => const AddFriendPage(),
-                ),
-              );
-            },
-            icon: const UIcon(
-              UIcons.add_contact_member,
-              color: UColors.textMed,
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              UBottomSheetOptions(
-                context,
-                sheetTitle: UAppStrings.moreOptions,
-                titleList: [
-                  UAppStrings.contactsIndexPage_friendRequests,
-                  UAppStrings.contactsIndexPage_outgoingRequests,
-                  UAppStrings.contactsIndexPage_blocked
-                ],
-                iconList: [
-                  UIcons.friend_requests,
-                  UIcons.outgoing_requests,
-                  UIcons.blocked_contacts
-                ],
-                onTapList: [
-                  () {
-                    Navigator.of(context, rootNavigator: true).pop();
-                    Navigator.of(context).push(
-                      MaterialPageRoute<void>(
-                        builder: (context) => const FriendRequestPage(),
-                      ),
-                    );
+      body: SafeArea(
+        child: FutureBuilder(
+          future: loadingContacts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final contactsList = snapshot.data! as List<MockContact>;
+
+              if (contactsList.isEmpty) {
+                return const NoFriendBody();
+              } else {
+                //Turn [MockContact] into AZItem(ISuspensionBean)
+                //which will be used in AZListView
+                final contactsAZList = contactsList
+                    .map(
+                      (item) => _AZItem(
+                          contact: item, tag: item.name[0].toUpperCase()),
+                    )
+                    .toList()
+                  ..sort(((a, b) => a.contact.name.compareTo(b.contact.name)));
+
+                //Sort tags include @ and #
+                SuspensionUtil.sortListBySuspensionTag(contactsAZList);
+
+                //Let each item know if it needs to show tag name above them
+                SuspensionUtil.setShowSuspensionStatus(contactsAZList);
+
+                return AzListView(
+                  data: contactsAZList,
+                  itemCount: contactsAZList.length,
+                  itemBuilder: (context, index) {
+                    final item = contactsAZList[index];
+                    if (index == 0) {
+                      return Column(
+                        children: [
+                          const _UAppBar(),
+                          _buildContactsList(item),
+                        ],
+                      );
+                    }
+                    return _buildContactsList(item);
                   },
-                  () {
-                    Navigator.of(context, rootNavigator: true).pop();
-                    Navigator.of(context).push(
-                      MaterialPageRoute<void>(
-                        builder: (context) => const OutgoingRequestPage(),
-                      ),
-                    );
-                  },
-                  () {
-                    Navigator.of(context, rootNavigator: true).pop();
-                    Navigator.of(context).push(
-                      MaterialPageRoute<void>(
-                        builder: (_) => const BlockedPage(),
-                      ),
-                    );
-                  }
-                ],
-              ).show();
-            },
-            icon: const UIcon(
-              UIcons.hamburger_menu,
-              color: UColors.textMed,
-            ),
-          )
-        ],
-      ),
-      body: FutureBuilder(
-        future: loadingContacts(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final contactsList = snapshot.data! as List<MockContact>;
-
-            if (contactsList.isEmpty) {
-              return const NoFriendBody();
-            } else {
-              //Turn [MockContact] into AZItem(ISuspensionBean)
-              //which will be used in AZListView
-              final contactsAZList = contactsList
-                  .map(
-                    (item) =>
-                        _AZItem(contact: item, tag: item.name[0].toUpperCase()),
-                  )
-                  .toList()
-                ..sort(((a, b) => a.contact.name.compareTo(b.contact.name)));
-
-              //Sort tags include @ and #
-              SuspensionUtil.sortListBySuspensionTag(contactsAZList);
-
-              //Let each item know if it needs to show tag name above them
-              SuspensionUtil.setShowSuspensionStatus(contactsAZList);
-
-              return AzListView(
-                data: contactsAZList,
-                itemCount: contactsAZList.length,
-                itemBuilder: (context, index) {
-                  final item = contactsAZList[index];
-                  return _buildContactsList(item);
-                },
-//indexBar is the initial letter on the right
-                indexBarMargin: const EdgeInsets.only(right: 8),
-                indexBarWidth: 16,
-                indexBarOptions: IndexBarOptions(
-                  needRebuild: true,
-                  textStyle: UTextStyle.B1_body.style
-                      .returnTextStyleType(color: UColors.textDark),
-                  selectTextStyle: UTextStyle.B3_bold.style.returnTextStyleType(
-                    color: Colors.white,
-                  ),
-                  selectItemDecoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: UColors.ctaBlue,
-                  ),
-                  indexHintAlignment: Alignment.centerRight,
-                  indexHintOffset: const Offset(20, 16),
-                ),
-//indexHint is the widget shows when user hold on the indexBar
-                indexHintBuilder: (context, hint) => Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      height: 40,
-                      width: 40,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: UColors.ctaBlue,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: UText(
-                        hint,
-                        textStyle: UTextStyle.H2_secondaryHeader,
-                        textColor: UColors.white,
-                      ),
+                  //indexBar is the initial letter on the right
+                  indexBarMargin: const EdgeInsets.only(right: 8),
+                  indexBarWidth: 16,
+                  indexBarOptions: IndexBarOptions(
+                    needRebuild: true,
+                    textStyle: UTextStyle.B1_body.style
+                        .returnTextStyleType(color: UColors.textDark),
+                    selectTextStyle:
+                        UTextStyle.B3_bold.style.returnTextStyleType(
+                      color: Colors.white,
                     ),
-                    CustomPaint(painter: Triangle()),
-                  ],
-                ),
-              );
+                    selectItemDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: UColors.ctaBlue,
+                    ),
+                    indexHintAlignment: Alignment.centerRight,
+                    indexHintOffset: const Offset(20, 16),
+                  ),
+                  //indexHint is the widget shows when user hold on the indexBar
+                  indexHintBuilder: (context, hint) => Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: UColors.ctaBlue,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: UText(
+                          hint,
+                          textStyle: UTextStyle.H2_secondaryHeader,
+                          textColor: UColors.white,
+                        ),
+                      ),
+                      CustomPaint(painter: Triangle()),
+                    ],
+                  ),
+                );
+              }
+            } else if (snapshot.hasError) {
+              log(snapshot.error.toString());
             }
-          } else if (snapshot.hasError) {
-            log(snapshot.error.toString());
-          }
-          // TODO(yijing): update to standard indicator
-          return const Center(child: ULoadingIndicator());
-        },
+            // TODO(yijing): update to standard indicator
+            return const Center(child: ULoadingIndicator());
+          },
+        ),
       ),
     );
   }
@@ -195,6 +121,101 @@ class ContactsIndexPage extends StatelessWidget {
           imageAddress: item.contact.imageAddress,
           onTap: () {},
         ),
+      ],
+    );
+  }
+}
+
+class _UAppBar extends StatelessWidget {
+  const _UAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return UAppBar.actions(
+      title: UAppStrings.contactsIndexPage_appBarTitle,
+      leading: IconButton(
+        icon: const UIcon(
+          UIcons.lefthand_navigation_drawer,
+          color: UColors.textMed,
+        ),
+        onPressed: () {},
+      ),
+      actionList: [
+        IconButton(
+          onPressed: () {
+            showCustomSearch(
+              context: context,
+              delegate: ContactSearch(loadingContacts()),
+            );
+          },
+          icon: const UIcon(
+            UIcons.search,
+            color: UColors.textMed,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (context) => const AddFriendPage(),
+              ),
+            );
+          },
+          icon: const UIcon(
+            UIcons.add_contact_member,
+            color: UColors.textMed,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            UBottomSheetOptions(
+              context,
+              sheetTitle: UAppStrings.moreOptions,
+              titleList: [
+                UAppStrings.contactsIndexPage_friendRequests,
+                UAppStrings.contactsIndexPage_outgoingRequests,
+                UAppStrings.contactsIndexPage_blocked
+              ],
+              iconList: [
+                UIcons.friend_requests,
+                UIcons.outgoing_requests,
+                UIcons.blocked_contacts
+              ],
+              onTapList: [
+                () {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                      builder: (context) => const FriendRequestPage(),
+                    ),
+                  );
+                },
+                () {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                      builder: (context) => const OutgoingRequestPage(),
+                    ),
+                  );
+                },
+                () {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                      builder: (_) => const BlockedPage(),
+                    ),
+                  );
+                }
+              ],
+            ).show();
+          },
+          icon: const UIcon(
+            UIcons.hamburger_menu,
+            color: UColors.textMed,
+          ),
+        )
       ],
     );
   }
