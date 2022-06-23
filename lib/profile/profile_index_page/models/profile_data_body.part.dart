a31d4b71f07689afbfa03a61a3cf5f19ef4431a0
lgmarchi <lucasgmarchi@gmail.com>
2022-06-23 15:41:23 -0300

    UL-88 - refactor(ProfileData): Adjust to be responsive in small screens

--- lib/profile/profile_index_page/models/profile_data_body.part.dart
+++ lib/profile/profile_index_page/models/profile_data_body.part.dart
@@ -11,93 +11,96 @@ class _ProfileData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: 104,
-          child: Column(
-            children: [
-              const UText(
-                UAppStrings.profileIndexPage_badges,
-                textStyle: UTextStyle.H5_fifthHeader,
-              ),
-              const SizedBox.square(
-                dimension: 8,
-              ),
-              SizedBox(
-                height: ((_badgesQuantity / 3).ceil() * 24) +
-                    ((_badgesQuantity / 3).floor() * 4),
-                width: (3 * 24) + (2 * 4),
-                child: CustomScrollView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  slivers: [
-                    SliverGrid.count(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 4,
-                      crossAxisSpacing: 4,
-                      children: List.generate(
-                        _badgesQuantity,
-                        (index) => Container(
-                          height: 24,
-                          width: 24,
-                          color: UColors.defGrey,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 104,
+            child: Column(
+              children: [
+                const UText(
+                  UAppStrings.profileIndexPage_badges,
+                  textStyle: UTextStyle.H5_fifthHeader,
+                ),
+                const SizedBox.square(
+                  dimension: 8,
+                ),
+                SizedBox(
+                  height: ((_badgesQuantity / 3).ceil() * 24) +
+                      ((_badgesQuantity / 3).floor() * 4),
+                  width: (3 * 24) + (2 * 4),
+                  child: CustomScrollView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    slivers: [
+                      SliverGrid.count(
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 4,
+                        crossAxisSpacing: 4,
+                        children: List.generate(
+                          _badgesQuantity,
+                          (index) => Container(
+                            height: 24,
+                            width: 24,
+                            color: UColors.defGrey,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        const SizedBox.square(
-          dimension: 8,
-        ),
-        SizedBox(
-          width: 104,
-          child: Column(
-            children: const [
-              UText(
-                UAppStrings.profileIndexPage_location,
-                textStyle: UTextStyle.H5_fifthHeader,
-              ),
-              SizedBox.square(
-                dimension: 8,
-              ),
-              UText(
-                UAppStrings.profileIndexPage_locationValue,
-                textStyle: UTextStyle.H5_fifthHeader,
-                textColor: UColors.white,
-              ),
-            ],
+          const SizedBox.square(
+            dimension: 8,
           ),
-        ),
-        const SizedBox.square(
-          dimension: 8,
-        ),
-        SizedBox(
-          width: 104,
-          child: Column(
-            children: const [
-              UText(
-                UAppStrings.profileIndexPage_friends,
-                textStyle: UTextStyle.H5_fifthHeader,
-              ),
-              SizedBox.square(
-                dimension: 8,
-              ),
-              UText(
-                UAppStrings.profileIndexPage_friendsNumber,
-                textStyle: UTextStyle.H5_fifthHeader,
-                textColor: UColors.white,
-              ),
-            ],
+          SizedBox(
+            width: 104,
+            child: Column(
+              children: const [
+                UText(
+                  UAppStrings.profileIndexPage_location,
+                  textStyle: UTextStyle.H5_fifthHeader,
+                ),
+                SizedBox.square(
+                  dimension: 8,
+                ),
+                UText(
+                  UAppStrings.profileIndexPage_locationValue,
+                  textStyle: UTextStyle.H5_fifthHeader,
+                  textColor: UColors.white,
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          const SizedBox.square(
+            dimension: 8,
+          ),
+          SizedBox(
+            width: 104,
+            child: Column(
+              children: const [
+                UText(
+                  UAppStrings.profileIndexPage_friends,
+                  textStyle: UTextStyle.H5_fifthHeader,
+                ),
+                SizedBox.square(
+                  dimension: 8,
+                ),
+                UText(
+                  UAppStrings.profileIndexPage_friendsNumber,
+                  textStyle: UTextStyle.H5_fifthHeader,
+                  textColor: UColors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
