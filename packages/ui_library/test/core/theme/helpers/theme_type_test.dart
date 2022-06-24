760e06a04d0507d51e3f15b56c771c88976ff862
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-06-24 20:31:35 +0700

    test(ThemeType): cover both extensions

--- packages/ui_library/test/core/theme/helpers/theme_type_test.dart
+++ packages/ui_library/test/core/theme/helpers/theme_type_test.dart
@@ -4,13 +4,27 @@ import 'package:ui_library/core/theme/helpers/theme_type.dart';
 void main() {
   group('Unit Test ThemeType |', () {});
 
-  test('Should return correct sizes for Theme Type', () {
+  test(
+      'Should return correct name for Theme Type when extension toStrName is called',
+      () {
     const _kDark = 'dark';
     const _kLight = 'light';
     const _kTbd = 'tbd';
 
-    expect(ThemeType.dark.name, _kDark);
-    expect(ThemeType.light.name, _kLight);
-    expect(ThemeType.tbd.name, _kTbd);
+    expect(ThemeType.dark.toStrName(), _kDark);
+    expect(ThemeType.light.toStrName(), _kLight);
+    expect(ThemeType.tbd.toStrName(), _kTbd);
+  });
+
+  test(
+      'Should return correct name for Theme Type when extension toThemeType is called',
+      () {
+    const _kDark = 'dark';
+    const _kLight = 'light';
+    const _kTbd = 'tbd';
+
+    expect(_kDark.toThemeType(), ThemeType.dark);
+    expect(_kLight.toThemeType(), ThemeType.light);
+    expect(_kTbd.toThemeType(), ThemeType.tbd);
   });
 }
