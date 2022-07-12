65cded93e0a4c17799c317723d64953d00739c40
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-07-12 19:17:18 +0700

    test(ui_library/core): improve naming

--- packages/ui_library/test/core/const/u_fonts_test.dart
+++ packages/ui_library/test/core/const/u_fonts_test.dart
@@ -3,17 +3,17 @@ import 'package:given_when_then_unit_test/given_when_then_unit_test.dart';
 import 'package:shouldly/shouldly.dart';
 
 void main() {
-  given('Default arguments', () {
+  given('UFonts Class - Font\'s name', () {
     const _iconsFont = 'UIcons';
     const _textPoppinsFont = 'Poppins';
     const _textSpaceMonoFont = 'SpaceMono';
 
-    then('Icon Font should be UICons',
+    then('Icon Font should be $_iconsFont',
         () => UFonts.iconsFont.should.be(_iconsFont),
         and: {
-          'Poppins Font should be Poppins': () =>
+          'Poppins Font should be $_textPoppinsFont': () =>
               UFonts.textPoppinsFont.should.be(_textPoppinsFont),
-          'Space Mono Font should be Space Mono': () =>
+          'Space Mono Font should be $_textSpaceMonoFont': () =>
               UFonts.textSpaceMonoFont.should.be(_textSpaceMonoFont),
         });
   });
