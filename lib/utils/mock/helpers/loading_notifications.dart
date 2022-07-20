ca6548d40dbe3709ddb49b8aea5d1681290a982b
Yijing Huang <hyj891204@gmail.com>
2022-07-20 12:28:50 -0500

    fix: fix the file path difference during rebase

--- lib/utils/mock/helpers/loading_notifications.dart
+++ lib/utils/mock/helpers/loading_notifications.dart
@@ -7,7 +7,7 @@ Future<List<MockNotification>> loadingNotifications() async {
   const hasFriends = true;
   if (hasFriends == true) {
     final jsonString = await rootBundle
-        .loadString('lib/utils/mock/data/mock_notifications_list.json');
+        .loadString('lib/utils/mock/data/json/mock_notifications_list.json');
     final list = await jsonDecode(jsonString) as List<dynamic>;
     return list.map(MockNotification.fromJson).toList();
   } else {
