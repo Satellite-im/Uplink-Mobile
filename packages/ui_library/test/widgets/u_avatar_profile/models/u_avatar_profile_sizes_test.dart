cbf8abc4679e535af73ee1dbc86af05a12a0f4ab
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-07-20 21:22:02 +0700

    test(ui_library): improve u_user_profile and u_avatar_profile\'s models unit test

--- packages/ui_library/test/widgets/u_avatar_profile/models/u_avatar_profile_sizes_test.dart
+++ packages/ui_library/test/widgets/u_avatar_profile/models/u_avatar_profile_sizes_test.dart
@@ -1,14 +1,17 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:ui_library/widgets/u_avatars_profile/models/u_avatar_profile_sizes.dart';
+import 'package:given_when_then_unit_test/given_when_then_unit_test.dart';
+import 'package:shouldly/shouldly.dart';
 
 void main() {
-  group('Unit Test UAvatarProfileSizes |', () {});
-
-  test('Should return correct sizes for Avatar Profiles Widgets', () {
+  given('UAvatarProfileSizes Class - Widget profile\'s sizes', () {
     const _largeSize = 56;
     const _normalSize = 40;
 
-    expect(UAvatarProfileSize.large.size, _largeSize);
-    expect(UAvatarProfileSize.normal.size, _normalSize);
+    then('Large size should be $_largeSize',
+        () => UAvatarProfileSize.large.size.should.be(_largeSize),
+        and: {
+          'Normal size should be $_normalSize': () =>
+              UAvatarProfileSize.normal.size.should.be(_normalSize),
+        });
   });
 }
