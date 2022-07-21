eedccc4c5ca8ec0afa8a3c6c1d8d6688e03626ad
Yijing Huang <hyj891204@gmail.com>
2022-07-21 17:30:19 -0500

    feat(UImageButton):add UImageButton and its showroom page

--- packages/ui_library/lib/widgets/buttons/u_image_button/u_image_button.dart
+++ packages/ui_library/lib/widgets/buttons/u_image_button/u_image_button.dart
@@ -1,1 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:ui_library/ui_library_export.dart';
 
+class UImageButton extends StatelessWidget {
+  const UImageButton({
+    Key? key,
+    required this.uImage,
+    this.isLinked,
+    this.isFavored,
+    this.isDeleting,
+  }) : super(key: key);
+
+  final UImage uImage;
+  final bool? isLinked;
+  final bool? isFavored;
+  final bool? isDeleting;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 168,
+      width: 160,
+      child: Stack(children: [
+        ClipRRect(
+          child: uImage,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        Align(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: [
+                if (isLinked == true)
+                  const Align(
+                    alignment: Alignment.topLeft,
+                    child: UIcon(
+                      UIcons.link_button,
+                      color: UColors.textMed,
+                    ),
+                  ),
+                if (isFavored == true)
+                  const Align(
+                    alignment: Alignment.topRight,
+                    child: UIcon(
+                      UIcons.favorite_button,
+                      color: UColors.textMed,
+                    ),
+                  ),
+                if (isDeleting == true)
+                  const Align(
+                    alignment: Alignment.bottomRight,
+                    child: UIcon(
+                      UIcons.remove,
+                      color: UColors.textMed,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- packages/ui_showroom/lib/ui_pages/src/file_widgets/u_image_button_page.dart
+++ packages/ui_showroom/lib/ui_pages/src/file_widgets/u_image_button_page.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:ui_library/ui_library_export.dart';
 
+List<Widget> uImageButtonList = const [
+  UImageButton(
+    uImage: UImage(),
+    isDeleting: true,
+    isFavored: true,
+    isLinked: true,
+  ),
+  UImageButton(
+    uImage: UImage(),
+    isFavored: true,
+  ),
+  UImageButton(
+    uImage: UImage(),
+    isLinked: true,
+  ),
+  UImageButton(
+    uImage: UImage(),
+    isDeleting: true,
+  ),
+];
+
 class UImageButtonPage extends StatelessWidget {
   const UImageButtonPage({Key? key}) : super(key: key);
   static const routeName = '/UImageButton';
@@ -10,10 +31,13 @@ class UImageButtonPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(routeName.substring(1)),
       ),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [],
-      )),
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: ListView.separated(
+            itemBuilder: (_, index) => Center(child: uImageButtonList[index]),
+            separatorBuilder: (_, index) => const SizedBox(height: 10),
+            itemCount: uImageButtonList.length),
+      ),
     );
   }
 }
