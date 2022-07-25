b5f0ae6d83426259c8961b82df5830f12f230925
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-07-25 20:39:35 +0700

    fix(ui-library): remove redundant text style total testing

--- packages/ui_library/test/widgets/u_text/models/u_text_style_test.dart
+++ packages/ui_library/test/widgets/u_text/models/u_text_style_test.dart
@@ -26,12 +26,11 @@ void main() {
         });
   });
   given('H2_secondaryHeader UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then(
+        'H2_secondaryHeader fontFamily should be correct',
+        () =>
+            UTextStyle.H2_secondaryHeader.style.fontFamily.should.be('Poppins'),
         and: {
-          'H2_secondaryHeader fontFamily should be correct': () => UTextStyle
-              .H2_secondaryHeader.style.fontFamily.should
-              .be('Poppins'),
           'H2_secondaryHeader fontSize should be correct': () =>
               UTextStyle.H2_secondaryHeader.style.fontSize.should.be(14.0),
           'H2_secondaryHeader fontWeight should be correct': () => UTextStyle
@@ -46,12 +45,11 @@ void main() {
         });
   });
   given('H3_tertiaryHeader UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then(
+        'H3_tertiaryHeader fontFamily should be correct',
+        () => UTextStyle.H3_tertiaryHeader.style.fontFamily.should
+            .be('SpaceMono'),
         and: {
-          'H3_tertiaryHeader fontFamily should be correct': () => UTextStyle
-              .H3_tertiaryHeader.style.fontFamily.should
-              .be('SpaceMono'),
           'H3_tertiaryHeader fontSize should be correct': () =>
               UTextStyle.H3_tertiaryHeader.style.fontSize.should.be(13.0),
           'H3_tertiaryHeader fontWeight should be correct': () => UTextStyle
@@ -66,12 +64,11 @@ void main() {
         });
   });
   given('H4_fourthHeader UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then(
+        'H4_fourthHeader fontFamily should be correct',
+        () =>
+            UTextStyle.H4_fourthHeader.style.fontFamily.should.be('SpaceMono'),
         and: {
-          'H4_fourthHeader fontFamily should be correct': () => UTextStyle
-              .H4_fourthHeader.style.fontFamily.should
-              .be('SpaceMono'),
           'H4_fourthHeader fontSize should be correct': () =>
               UTextStyle.H4_fourthHeader.style.fontSize.should.be(13.0),
           'H4_fourthHeader fontWeight should be correct': () => UTextStyle
@@ -85,11 +82,9 @@ void main() {
         });
   });
   given('H5_fifthHeader UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then('H5_fifthHeader fontFamily should be correct',
+        () => UTextStyle.H5_fifthHeader.style.fontFamily.should.be('Poppins'),
         and: {
-          'H5_fifthHeader fontFamily should be correct': () =>
-              UTextStyle.H5_fifthHeader.style.fontFamily.should.be('Poppins'),
           'H5_fifthHeader fontSize should be correct': () =>
               UTextStyle.H5_fifthHeader.style.fontSize.should.be(12.0),
           'H5_fifthHeader fontWeight should be correct': () => UTextStyle
@@ -103,11 +98,9 @@ void main() {
         });
   });
   given('B1_body UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then('B1_body fontFamily should be correct',
+        () => UTextStyle.B1_body.style.fontFamily.should.be('Poppins'),
         and: {
-          'B1_body fontFamily should be correct': () =>
-              UTextStyle.B1_body.style.fontFamily.should.be('Poppins'),
           'B1_body fontSize should be correct': () =>
               UTextStyle.B1_body.style.fontSize.should.be(12.0),
           'B1_body fontWeight should be correct': () =>
@@ -119,11 +112,9 @@ void main() {
         });
   });
   given('B2_medium UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then('B2_medium fontFamily should be correct',
+        () => UTextStyle.B2_medium.style.fontFamily.should.be('Poppins'),
         and: {
-          'B2_medium fontFamily should be correct': () =>
-              UTextStyle.B2_medium.style.fontFamily.should.be('Poppins'),
           'B2_medium fontSize should be correct': () =>
               UTextStyle.B2_medium.style.fontSize.should.be(12.0),
           'B2_medium fontWeight should be correct': () =>
@@ -135,11 +126,9 @@ void main() {
         });
   });
   given('B3_bold UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then('B3_bold fontFamily should be correct',
+        () => UTextStyle.B3_bold.style.fontFamily.should.be('Poppins'),
         and: {
-          'B3_bold fontFamily should be correct': () =>
-              UTextStyle.B3_bold.style.fontFamily.should.be('Poppins'),
           'B3_bold fontSize should be correct': () =>
               UTextStyle.B3_bold.style.fontSize.should.be(12.0),
           'B3_bold fontWeight should be correct': () =>
@@ -151,11 +140,9 @@ void main() {
         });
   });
   given('B4_italics UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then('B4_italics fontFamily should be correct',
+        () => UTextStyle.B4_italics.style.fontFamily.should.be('Poppins'),
         and: {
-          'B4_italics fontFamily should be correct': () =>
-              UTextStyle.B4_italics.style.fontFamily.should.be('Poppins'),
           'B4_italics fontSize should be correct': () =>
               UTextStyle.B4_italics.style.fontSize.should.be(12.0),
           'B4_italics fontWeight should be correct': () =>
@@ -167,11 +154,9 @@ void main() {
         });
   });
   given('M1_micro UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then('M1_micro fontFamily should be correct',
+        () => UTextStyle.M1_micro.style.fontFamily.should.be('Poppins'),
         and: {
-          'M1_micro fontFamily should be correct': () =>
-              UTextStyle.M1_micro.style.fontFamily.should.be('Poppins'),
           'M1_micro fontSize should be correct': () =>
               UTextStyle.M1_micro.style.fontSize.should.be(10.0),
           'M1_micro fontWeight should be correct': () =>
@@ -183,12 +168,11 @@ void main() {
         });
   });
   given('BUT1_primaryButton UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then(
+        'BUT1_primaryButton fontFamily should be correct',
+        () =>
+            UTextStyle.BUT1_primaryButton.style.fontFamily.should.be('Poppins'),
         and: {
-          'BUT1_primaryButton fontFamily should be correct': () => UTextStyle
-              .BUT1_primaryButton.style.fontFamily.should
-              .be('Poppins'),
           'BUT1_primaryButton fontSize should be correct': () =>
               UTextStyle.BUT1_primaryButton.style.fontSize.should.be(12.0),
           'BUT1_primaryButton fontWeight should be correct': () => UTextStyle
@@ -203,12 +187,11 @@ void main() {
         });
   });
   given('BUT2_secondaryButton UTextStyles - text style\'s values', () {
-    then('Number of text styles should be 12',
-        () => UTextStyle.values.length.should.be(12),
+    then(
+        'BUT2_secondaryButton fontFamily should be correct',
+        () => UTextStyle.BUT2_secondaryButton.style.fontFamily.should
+            .be('Poppins'),
         and: {
-          'BUT2_secondaryButton fontFamily should be correct': () => UTextStyle
-              .BUT2_secondaryButton.style.fontFamily.should
-              .be('Poppins'),
           'BUT2_secondaryButton fontSize should be correct': () =>
               UTextStyle.BUT2_secondaryButton.style.fontSize.should.be(12.0),
           'BUT2_secondaryButton fontWeight should be correct': () => UTextStyle
