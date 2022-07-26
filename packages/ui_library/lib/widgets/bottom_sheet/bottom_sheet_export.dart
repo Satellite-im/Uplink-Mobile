59bce39578a9722a58035e125ed487e4ed2253c0
Yijing Huang <hyj891204@gmail.com>
2022-07-26 11:38:59 -0500

    refactor(UBottomSheet): extract UHomeIndicator for reusing in other widgets

--- packages/ui_library/lib/widgets/bottom_sheet/bottom_sheet_export.dart
+++ packages/ui_library/lib/widgets/bottom_sheet/bottom_sheet_export.dart
@@ -2,3 +2,4 @@ export 'u_bottom_sheet_two_buttons.dart';
 export 'u_bottom_sheet_pin.dart';
 export 'u_bottom_sheet_options.dart';
 export 'u_bottom_sheet_notifications.dart';
+export 'u_home_indicator.dart';
