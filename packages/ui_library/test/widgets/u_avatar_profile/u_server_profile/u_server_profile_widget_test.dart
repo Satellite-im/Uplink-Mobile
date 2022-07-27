9ec84125614668a13e6ebc7fc66de5a007d37c68
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-07-27 21:48:04 +0700

    test(ui-library): add failing unit test (WIP)

--- packages/ui_library/test/widgets/u_avatar_profile/u_server_profile/u_server_profile_widget_test.dart
+++ packages/ui_library/test/widgets/u_avatar_profile/u_server_profile/u_server_profile_widget_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ui_library/widgets/u_avatars_profile/u_server_profile/u_server_profile.dart';
+import 'package:ui_library/widgets/global/placeholder.dart';
+import 'dart:developer';
 
 void main() {
   group('Widget Test UServerProfile |', () {
@@ -27,6 +29,78 @@ void main() {
       );
 
       final Size _baseSize = tester.getSize(find.byType(UServerProfile));
+      expect(_baseSize.width, equals(40.0));
+      expect(_baseSize.height, equals(40.0));
+    });
+  });
+  group('Widget Test UServerProfile with null imagePath |', () {
+    testWidgets('Should return the right size value',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      UServerProfile(
+                        imagePath: null,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      final Size _baseSize = tester.getSize(find.byType(UServerProfile));
+      final _container = find.byWidgetPredicate(
+        ((widget) => (widget is ClipOval && widget.child != null)),
+      );
+
+      expect(_container, findsOneWidget);
+
+      expect(_baseSize.width, equals(40.0));
+      expect(_baseSize.height, equals(40.0));
+    });
+  });
+  group('Widget Test UServerProfile with custom imagePath |', () {
+    testWidgets('Should return the right size value',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      UServerProfile(
+                        imagePath: 'https://satellite.im/images/logo.webp',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      final Size _baseSize = tester.getSize(find.byType(UServerProfile));
+      final _container = find.byWidgetPredicate(
+        ((widget) => (widget is ClipOval && widget.child != null)),
+      );
+
+      expect(_container, findsOneWidget);
+
       expect(_baseSize.width, equals(40.0));
       expect(_baseSize.height, equals(40.0));
     });
