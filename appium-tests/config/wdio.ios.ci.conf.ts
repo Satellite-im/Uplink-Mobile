f1cc4a1044347f6b084e7bc093ee87b7d594eaac
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2022-08-04 16:39:50 -0500

    chore(appium): add test file to create PR pt4

--- appium-tests/config/wdio.ios.ci.conf.ts
+++ appium-tests/config/wdio.ios.ci.conf.ts
@@ -25,7 +25,7 @@ config.capabilities = [
         // The path to the app
         "appium:app": join(
             process.cwd(),
-            "./apps/Runner.app"
+            "./apps/iosApp.ipa"
         ),
         //"appName": "Uplink",
         "appium:newCommandTimeout": 240,
