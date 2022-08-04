d8ca66874d79ac5c79174e567d35aa2ec0e367a1
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-08-04 18:21:02 +0700

    chore(ui-library): remove unused comments

---
+++ test/utils/services/ui_utils/loading_page_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:uplink/utils/ui_utils/loading_page.dart';
+
+void main() {
+  group('Widget Test Loading Page |', () {
+    testWidgets('Should return the right size value',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      LoadingPage(title: 'title', body: 'body'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      final Size _baseSize = tester.getSize(find.byType(LoadingPage));
+      expect(_baseSize.width, equals(40.0));
+      expect(_baseSize.height, equals(40.0));
+    });
+  });
+}
