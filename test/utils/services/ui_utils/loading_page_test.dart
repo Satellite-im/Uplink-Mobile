025763976ad96153b1e0c67d4523e06d68675efc
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-08-05 21:52:23 +0700

    chore(utils): add trailing commas

--- test/utils/services/ui_utils/loading_page_test.dart
+++ test/utils/services/ui_utils/loading_page_test.dart
@@ -3,24 +3,31 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:uplink/utils/ui_utils/loading_page.dart';
 
 void main() {
-  group('Widget Test Loading Page |', () {
-    testWidgets('Should return the right size value',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: Column(mainAxisSize: MainAxisSize.min, children: const [
-                LoadingPage(title: 'title', body: 'body'),
-              ]),
+  group(
+    'Widget Test Loading Page |',
+    () {
+      testWidgets('Should return the right size value',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    LoadingPage(title: 'title', body: 'body'),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      );
+        );
 
-      final _baseSize = tester.getSize(find.byType(LoadingPage));
-      expect(_baseSize.width, equals(40.0));
-      // expect(find.text('body'), findsOneWidget);
-    });
-  }, skip: true);
+        final _baseSize = tester.getSize(find.byType(LoadingPage));
+        expect(_baseSize.width, equals(40.0));
+        // expect(find.text('body'), findsOneWidget);
+      });
+    },
+    skip: true,
+  );
 }
