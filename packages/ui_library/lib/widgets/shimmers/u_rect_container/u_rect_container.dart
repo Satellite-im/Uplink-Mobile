7af9124571c652993d093653df7b7290ff95cd61
Yijing Huang <hyj891204@gmail.com>
2022-08-05 13:37:04 -1000

    update(URectContainer):add margin property

--- packages/ui_library/lib/widgets/shimmers/u_rect_container/u_rect_container.dart
+++ packages/ui_library/lib/widgets/shimmers/u_rect_container/u_rect_container.dart
@@ -7,15 +7,18 @@ class URectContainer extends StatelessWidget {
     this.height,
     this.width,
     this.bordRadius,
+    this.margin,
   }) : super(key: key);
 
   final double? height;
   final double? width;
   final BorderRadiusGeometry? bordRadius;
+  final EdgeInsetsGeometry? margin;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: margin,
       height: height,
       width: width,
       decoration: BoxDecoration(
