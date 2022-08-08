eb98631046f4d66e7abdcca8ce06768dcb2a1111
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-08-08 23:29:40 +0700

    test(ui-library): increase coverage for UTextInput

--- packages/ui_library/test/widgets/text_input/u_text_input_test.dart
+++ packages/ui_library/test/widgets/text_input/u_text_input_test.dart
@@ -11,11 +11,12 @@ void main() {
           home: Scaffold(
             body: Center(
               child: Column(mainAxisSize: MainAxisSize.min, children: [
-                Flexible(
-                    child: UTextInput.singleLine(
-                  hintText: 'hint text',
-                  onChanged: ((p0) => 'hint string'),
-                ))
+                Column(children: [
+                  UTextInput.singleLine(
+                    hintText: 'hint text',
+                    onChanged: ((p0) => 'hint string'),
+                  )
+                ])
               ]),
             ),
           ),
@@ -25,7 +26,84 @@ void main() {
       final Size _baseSize = tester.getSize(find.byType(UTextInput));
       await tester.enterText(find.byType(UTextInput), 'second text');
       await tester.pumpAndSettle();
-      expect(_baseSize.height, equals(600.0));
+      expect(_baseSize.height, equals(56.0));
+      expect(find.text('second text'), findsOneWidget);
+    });
+    testWidgets('Should return the right size for single line with title',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                Column(children: [
+                  UTextInput.singleLineWithTitle(
+                    hintText: 'hint text',
+                    onChanged: ((p0) => 'hint string'),
+                    textFieldTitle: 'title text field',
+                  )
+                ])
+              ]),
+            ),
+          ),
+        ),
+      );
+
+      final Size _baseSize = tester.getSize(find.byType(UTextInput));
+      await tester.enterText(find.byType(UTextInput), 'second text');
+      await tester.pumpAndSettle();
+      expect(_baseSize.height, equals(81.0));
+      expect(find.text('second text'), findsOneWidget);
+    });
+    testWidgets('Should return the right size for multiline',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                Column(children: [
+                  UTextInput.multiLines(
+                    hintText: 'hint text',
+                    onChanged: ((p0) => 'hint string'),
+                  )
+                ])
+              ]),
+            ),
+          ),
+        ),
+      );
+
+      final Size _baseSize = tester.getSize(find.byType(UTextInput));
+      await tester.enterText(find.byType(UTextInput), 'second text');
+      await tester.pumpAndSettle();
+      expect(_baseSize.height, equals(87.0));
+      expect(find.text('second text'), findsOneWidget);
+    });
+    testWidgets('Should return the right size for multiline with title',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                Column(children: [
+                  UTextInput.multiLinesWithTitle(
+                    hintText: 'hint text',
+                    onChanged: ((p0) => 'hint string'),
+                    textFieldTitle: 'title text field',
+                  )
+                ])
+              ]),
+            ),
+          ),
+        ),
+      );
+
+      final Size _baseSize = tester.getSize(find.byType(UTextInput));
+      await tester.enterText(find.byType(UTextInput), 'second text');
+      await tester.pumpAndSettle();
+      expect(_baseSize.height, equals(112.0));
       expect(find.text('second text'), findsOneWidget);
     });
   });
