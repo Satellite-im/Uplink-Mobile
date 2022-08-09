d6f1ee3e4b5ca7f31603bb44fef8f87d325f7b7b
lgmarchi <lucasgmarchi@gmail.com>
2022-08-09 09:52:00 -0300

    UL-161 - refactor(Warp); Let it compatible with the last version of warp

--- lib/utils/warp/warp.dart
+++ lib/utils/warp/warp.dart
@@ -1,33 +1,46 @@
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:warp_dart/warp_dart.dart' as warp;
+import 'package:warp_dart/mp_ipfs.dart' as mp_ipfs;
+import 'package:warp_dart/multipass.dart' as multipass;
+import 'package:warp_dart/warp.dart' as warp;
 
 warp.DID? did;
 
 // TODO(Lucas): Warp is here and the profile pages just for Mock purposes, change these things later
 class Warp {
-  final multipass = warp.multipassIpfsTemporary(warp.newTesseractInstance());
+  late multipass.MultiPass? _mpIpfs;
+
+  final store = warp.Tesseract.newStore();
 
   void createUser({required String username, String messageStatus = ''}) {
-    did = multipass.createIdentity(username, 'about about about');
-    multipass.modifyStatus(messageStatus);
+    store.unlock('Hello');
+
+    _mpIpfs = mp_ipfs.multipass_ipfs_temporary(store);
+    did = _mpIpfs!.createIdentity(username, 'secured_phrase');
+    final _identityUpdated =
+        multipass.IdentityUpdate.setStatusMessage(messageStatus);
+    _mpIpfs!.updateIdentity(_identityUpdated);
   }
 
   String getUsername() {
-    return multipass.getUsername(did!);
+    return _mpIpfs!.getOwnIdentity().username;
   }
 
   String changeUsername(String newUserName) {
-    multipass.modifyName(newUserName);
-    return getUsername();
+    multipass.IdentityUpdate.setUsername(newUserName);
+    final _identityUpdated = multipass.IdentityUpdate.setUsername(newUserName);
+    _mpIpfs!.updateIdentity(_identityUpdated);
+    return _mpIpfs!.getOwnIdentity().username;
   }
 
   String changeMessageStatus(String newStatus) {
-    multipass.modifyStatus(newStatus);
-    return getMessageStatus();
+    final _identityUpdated =
+        multipass.IdentityUpdate.setStatusMessage(newStatus);
+    _mpIpfs!.updateIdentity(_identityUpdated);
+    return _mpIpfs!.getOwnIdentity().status_message;
   }
 
   String getMessageStatus() {
-    return multipass.getStatus(did!);
+    return _mpIpfs!.getOwnIdentity().status_message;
   }
 }
