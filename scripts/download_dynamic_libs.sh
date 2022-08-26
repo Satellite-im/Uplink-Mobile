173c28790434518ac351b33658443d469f3f4476
lgmarchi <lucasgmarchi@gmail.com>
2022-08-26 15:36:11 -0300

    UL-176 - chore: Download warp libs in script

---
+++ scripts/download_dynamic_libs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/Satellite-im/warp-dart/main/android/src/main/jniLibs/arm64-v8a/libwarp.so -P android/app/src/main/jniLibs/arm64-v8a
+wget https://raw.githubusercontent.com/Satellite-im/warp-dart/main/android/src/main/jniLibs/arm64-v8a/libwarp_mp_ipfs.so -P android/app/src/main/jniLibs/arm64-v8a
