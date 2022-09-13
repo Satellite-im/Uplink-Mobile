b9a9b256d37ba3202999b8df28fff0443a7e3fd0
lgmarchi <lucasgmarchi@gmail.com>
2022-09-13 12:53:01 -0300

    UL-181 - refactor(UIcons): Add mute_notifications and info icon

--- packages/ui_library/lib/widgets/u_icon/models/u_icons.dart
+++ packages/ui_library/lib/widgets/u_icon/models/u_icons.dart
@@ -26,151 +26,155 @@ class UIcons {
   static const UIconData add_note =
       UIconData(0xe905, uFontFamily: _kIconsFontFamily);
   static const UIconData apple_watch =
-      UIconData(0xe90b, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe906, uFontFamily: _kIconsFontFamily);
   static const UIconData audio_off =
-      UIconData(0xe90c, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe907, uFontFamily: _kIconsFontFamily);
   static const UIconData audio_on =
-      UIconData(0xe90d, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe908, uFontFamily: _kIconsFontFamily);
   static const UIconData back_arrow =
-      UIconData(0xe90e, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe909, uFontFamily: _kIconsFontFamily);
   static const UIconData blocked_contacts =
-      UIconData(0xe90f, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe90a, uFontFamily: _kIconsFontFamily);
   static const UIconData camera =
-      UIconData(0xe910, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe90b, uFontFamily: _kIconsFontFamily);
   static const UIconData chatbar_emojis =
-      UIconData(0xe911, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe90c, uFontFamily: _kIconsFontFamily);
   static const UIconData chatbar_gifs =
-      UIconData(0xe912, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe90d, uFontFamily: _kIconsFontFamily);
   static const UIconData chatbar_send =
-      UIconData(0xe913, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe90e, uFontFamily: _kIconsFontFamily);
   static const UIconData chatbar_stickers =
-      UIconData(0xe914, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe90f, uFontFamily: _kIconsFontFamily);
   static const UIconData checkmark_1 =
-      UIconData(0xe915, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe910, uFontFamily: _kIconsFontFamily);
   static const UIconData checkmark_2 =
-      UIconData(0xe916, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe911, uFontFamily: _kIconsFontFamily);
   static const UIconData close =
-      UIconData(0xe917, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe912, uFontFamily: _kIconsFontFamily);
   static const UIconData compose_message =
-      UIconData(0xe918, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe913, uFontFamily: _kIconsFontFamily);
   static const UIconData copy_or_clone_button =
-      UIconData(0xe919, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe914, uFontFamily: _kIconsFontFamily);
   static const UIconData desktop =
-      UIconData(0xe91a, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe915, uFontFamily: _kIconsFontFamily);
   static const UIconData developer =
-      UIconData(0xe91b, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe916, uFontFamily: _kIconsFontFamily);
   static const UIconData devices =
-      UIconData(0xe91c, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe917, uFontFamily: _kIconsFontFamily);
   static const UIconData document =
-      UIconData(0xe91d, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe918, uFontFamily: _kIconsFontFamily);
   static const UIconData download =
-      UIconData(0xe91e, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe919, uFontFamily: _kIconsFontFamily);
   static const UIconData dropdown_menu =
-      UIconData(0xe91f, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe91a, uFontFamily: _kIconsFontFamily);
   static const UIconData encrypted =
-      UIconData(0xe920, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe91b, uFontFamily: _kIconsFontFamily);
   static const UIconData end_call =
-      UIconData(0xe921, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe91c, uFontFamily: _kIconsFontFamily);
   static const UIconData end_video_call =
-      UIconData(0xe922, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe91d, uFontFamily: _kIconsFontFamily);
   static const UIconData favorite =
-      UIconData(0xe923, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe91e, uFontFamily: _kIconsFontFamily);
   static const UIconData forward_arrow =
-      UIconData(0xe924, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe91f, uFontFamily: _kIconsFontFamily);
   static const UIconData friend_added =
-      UIconData(0xe925, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe920, uFontFamily: _kIconsFontFamily);
   static const UIconData friend_requests =
-      UIconData(0xe926, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe921, uFontFamily: _kIconsFontFamily);
   static const UIconData general =
-      UIconData(0xe927, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe922, uFontFamily: _kIconsFontFamily);
   static const UIconData go_back =
-      UIconData(0xe928, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe923, uFontFamily: _kIconsFontFamily);
   static const UIconData grid_view =
-      UIconData(0xe929, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe924, uFontFamily: _kIconsFontFamily);
   static const UIconData hamburger_menu =
-      UIconData(0xe92a, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe925, uFontFamily: _kIconsFontFamily);
   static const UIconData hide =
-      UIconData(0xe92b, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe926, uFontFamily: _kIconsFontFamily);
   static const UIconData image =
-      UIconData(0xe92c, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe927, uFontFamily: _kIconsFontFamily);
   static const UIconData import =
-      UIconData(0xe92d, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe928, uFontFamily: _kIconsFontFamily);
+  static const UIconData info =
+      UIconData(0xe929, uFontFamily: _kIconsFontFamily);
   static const UIconData laptop =
-      UIconData(0xe92e, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe92a, uFontFamily: _kIconsFontFamily);
   static const UIconData lefthand_navigation_drawer =
-      UIconData(0xe92f, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe92b, uFontFamily: _kIconsFontFamily);
   static const UIconData link =
-      UIconData(0xe930, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe92c, uFontFamily: _kIconsFontFamily);
   static const UIconData list_view =
-      UIconData(0xe931, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe92d, uFontFamily: _kIconsFontFamily);
   static const UIconData loading =
-      UIconData(0xe932, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe92e, uFontFamily: _kIconsFontFamily);
   static const UIconData location =
-      UIconData(0xe933, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe92f, uFontFamily: _kIconsFontFamily);
   static const UIconData logout =
-      UIconData(0xe934, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe930, uFontFamily: _kIconsFontFamily);
   static const UIconData menu_bar_contacts =
-      UIconData(0xe935, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe931, uFontFamily: _kIconsFontFamily);
   static const UIconData menu_bar_files =
-      UIconData(0xe936, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe932, uFontFamily: _kIconsFontFamily);
   static const UIconData menu_bar_home =
-      UIconData(0xe937, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe933, uFontFamily: _kIconsFontFamily);
   static const UIconData message =
-      UIconData(0xe938, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe934, uFontFamily: _kIconsFontFamily);
   static const UIconData mobile_phone =
-      UIconData(0xe939, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe935, uFontFamily: _kIconsFontFamily);
   static const UIconData multiple_accounts =
-      UIconData(0xe93a, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe936, uFontFamily: _kIconsFontFamily);
   static const UIconData mute =
-      UIconData(0xe93b, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe937, uFontFamily: _kIconsFontFamily);
+  static const UIconData mute_notifications =
+      UIconData(0xe938, uFontFamily: _kIconsFontFamily);
   static const UIconData notifications =
-      UIconData(0xe93c, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe939, uFontFamily: _kIconsFontFamily);
   static const UIconData outgoing_requests =
-      UIconData(0xe93d, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe93a, uFontFamily: _kIconsFontFamily);
   static const UIconData pin_entry_backspace =
-      UIconData(0xe93e, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe93b, uFontFamily: _kIconsFontFamily);
   static const UIconData pinned_message =
-      UIconData(0xe93f, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe93c, uFontFamily: _kIconsFontFamily);
   static const UIconData qr_code =
-      UIconData(0xe940, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe93d, uFontFamily: _kIconsFontFamily);
   static const UIconData qr_code_scanner_border =
-      UIconData(0xe941, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe93e, uFontFamily: _kIconsFontFamily);
   static const UIconData refresh =
-      UIconData(0xe942, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe93f, uFontFamily: _kIconsFontFamily);
   static const UIconData remove =
-      UIconData(0xe943, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe940, uFontFamily: _kIconsFontFamily);
   static const UIconData remove_friend =
-      UIconData(0xe944, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe941, uFontFamily: _kIconsFontFamily);
   static const UIconData report =
-      UIconData(0xe945, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe942, uFontFamily: _kIconsFontFamily);
   static const UIconData reveal =
-      UIconData(0xe946, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe943, uFontFamily: _kIconsFontFamily);
   static const UIconData right_arrow_inline_row =
-      UIconData(0xe947, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe944, uFontFamily: _kIconsFontFamily);
   static const UIconData search =
-      UIconData(0xe948, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe945, uFontFamily: _kIconsFontFamily);
   static const UIconData security =
-      UIconData(0xe949, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe946, uFontFamily: _kIconsFontFamily);
   static const UIconData select_box =
-      UIconData(0xe94a, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe947, uFontFamily: _kIconsFontFamily);
   static const UIconData setting =
-      UIconData(0xe94b, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe948, uFontFamily: _kIconsFontFamily);
   static const UIconData swap_camera =
-      UIconData(0xe94c, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe949, uFontFamily: _kIconsFontFamily);
   static const UIconData tablet =
-      UIconData(0xe94d, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe94a, uFontFamily: _kIconsFontFamily);
   static const UIconData unblock =
-      UIconData(0xe94e, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe94b, uFontFamily: _kIconsFontFamily);
   static const UIconData unencrypted =
-      UIconData(0xe94f, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe94c, uFontFamily: _kIconsFontFamily);
   static const UIconData unmute =
-      UIconData(0xe950, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe94d, uFontFamily: _kIconsFontFamily);
   static const UIconData user_profile =
-      UIconData(0xe951, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe94e, uFontFamily: _kIconsFontFamily);
   static const UIconData video_call =
-      UIconData(0xe952, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe94f, uFontFamily: _kIconsFontFamily);
   static const UIconData video_play =
-      UIconData(0xe953, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe950, uFontFamily: _kIconsFontFamily);
   static const UIconData voice_call =
-      UIconData(0xe954, uFontFamily: _kIconsFontFamily);
+      UIconData(0xe951, uFontFamily: _kIconsFontFamily);
 }
