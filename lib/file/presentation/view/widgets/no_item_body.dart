8735aadbf19ed03bae3039939c843f6b1d1aa5e0
Yijing Huang <hyj891204@gmail.com>
2022-09-29 19:02:54 -0500

    feat:add uploading photo from gallary feature and set rear camera as default when uploading from camera

--- lib/file/presentation/view/widgets/no_item_body.dart
+++ lib/file/presentation/view/widgets/no_item_body.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ui_library/ui_library_export.dart';
 import 'package:uplink/file/domain/item.dart';
 import 'package:uplink/file/presentation/controller/item_list_bloc.dart';
@@ -55,7 +56,48 @@ class _NoItemBodyState extends State<NoItemBody> {
                 firstButtonOnPressed: () async {
                   final _imageFile = await UImagePicker(
                     shouldShowPermissionDialog: true,
-                  ).pickImageFromCamera(context, uCropStyle: UCropStyle.none);
+                  ).pickImageFromCamera(
+                    context,
+                    uCropStyle: UCropStyle.none,
+                    device: CameraDevice.rear,
+                  );
+
+                  if (_imageFile != null) {
+                    //close bottem sheet and navigate to name page
+                    if (!mounted) return;
+                    final _fileName = await Navigator.of(context)
+                        .popAndPushNamed('/NameFilePage') as String?;
+                    //compose the photo as thumbnail after getting file name
+                    if (_fileName != null) {
+                      final _uint8List =
+                          await FlutterImageCompress.compressWithFile(
+                        _imageFile.path,
+                        quality: 10,
+                      );
+                      if (_uint8List != null) {
+                        final _base64String = base64Encode(_uint8List);
+                        final _item = Item(
+                          name: _fileName,
+                          thumbnail: _base64String,
+                          type: ItemType.photo,
+                          size: _imageFile.lengthSync(),
+                          creation: DateTime.now(),
+                          modified: DateTime.now(),
+                        );
+                        _itemListController.add(UploadItem(item: _item));
+                      }
+                    }
+                  } else {
+                    // close the bottom sheet if user didn't upload photo
+                    if (!mounted) return;
+                    Navigator.pop(context);
+                  }
+                },
+                secondButtonOnPressed: () async {
+                  // similar code as uploading from camera
+                  final _imageFile = await UImagePicker(
+                    shouldShowPermissionDialog: true,
+                  ).pickImageFromGallery(context, uCropStyle: UCropStyle.none);
 
                   if (_imageFile != null) {
                     //close bottem sheet and navigate to name page
@@ -88,7 +130,6 @@ class _NoItemBodyState extends State<NoItemBody> {
                     Navigator.pop(context);
                   }
                 },
-                secondButtonOnPressed: () {},
               ).show();
             },
           ),
