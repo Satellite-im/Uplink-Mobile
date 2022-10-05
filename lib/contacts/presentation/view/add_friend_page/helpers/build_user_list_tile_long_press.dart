50bc1d309a9a6b12fc336e0e58199f6acbac9162
lgmarchi <lucasgmarchi@gmail.com>
2022-10-05 18:40:54 -0300

    117- feat(BlockUser): Add block/unblock functions on AddFriendPage

--- lib/contacts/presentation/view/add_friend_page/helpers/build_user_list_tile_long_press.dart
+++ lib/contacts/presentation/view/add_friend_page/helpers/build_user_list_tile_long_press.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:ui_library/ui_library_export.dart';
 import 'package:uplink/chat/presentation/view/chat_room_page/chat_room_page.dart';
+import 'package:uplink/contacts/presentation/controller/friend_bloc.dart';
 import 'package:uplink/contacts/presentation/view/add_friend_page/models/models_export.dart';
 import 'package:uplink/contacts/presentation/view/user_profile_page/models/models_export.dart';
 import 'package:uplink/l10n/main_app_strings.dart';
 import 'package:uplink/shared/domain/entities/user.entity.dart';
 
 void buildUserListTileLongPress(BuildContext context, User user) {
+  final _friendController = GetIt.I.get<FriendBloc>();
   UBottomSheetOptions(
     context,
     sheetTitle: UAppStrings.moreOptions,
@@ -57,33 +60,33 @@ void buildUserListTileLongPress(BuildContext context, User user) {
         );
       },
       () {
-        final userNotifier = context.read<UserNotifier>();
         Navigator.of(context, rootNavigator: true).pop();
         showDialog<void>(
           context: context,
           builder: (context) {
-            return ChangeNotifierProvider.value(
-              value: userNotifier,
-              child: UDialogUserProfile(
-                bodyText: UAppStrings.blockDialogQ,
-                buttonText: UAppStrings.block,
-                popButtonText: UAppStrings.goBackButton,
-                onTap: () {
-                  userNotifier.blockFriend();
-                  Navigator.of(context).pop();
-                },
-                username: user.username,
-                uImage: UImage(
-                  imagePath: user.profilePicture == null
-                      ? ''
-                      : user.profilePicture!.path,
-                  imageSource: ImageSource.local,
-                ),
-                statusMessage: user.statusMessage,
+            return UDialogUserProfile(
+              bodyText: UAppStrings.blockDialogQ,
+              buttonText: UAppStrings.block,
+              popButtonText: UAppStrings.goBackButton,
+              onTap: () {
+                _friendController.add(BlockUserStarted(user));
+                Navigator.of(context).pop();
+              },
+              username: user.username,
+              uImage: UImage(
+                imagePath: user.profilePicture == null
+                    ? ''
+                    : user.profilePicture!.path,
+                imageSource: ImageSource.local,
               ),
+              statusMessage: user.statusMessage,
             );
           },
-        );
+        ).then((value) {
+          _friendController.add(
+            SearchUserStarted(userDid: user.did!),
+          );
+        });
       },
       // TODO(yijing): add report pages
       () {},
