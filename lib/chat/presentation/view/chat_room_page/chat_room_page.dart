78659baaa1f0ecb05609847138ac1d11b69cfd7d
lgmarchi <lucasgmarchi@gmail.com>
2022-10-31 13:30:47 -0300

    140 - refactor(imports): Remove unnecessary import

--- lib/chat/presentation/view/chat_room_page/chat_room_page.dart
+++ lib/chat/presentation/view/chat_room_page/chat_room_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ui_library/ui_library_export.dart';
